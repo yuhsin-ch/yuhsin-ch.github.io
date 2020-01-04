ac3dbfab609fcf7c85f54f6f95839e14a6d620b3
yuhsin-ch <58165367+yuhsin-ch@users.noreply.github.com>
2020-01-04 14:07:35 +0800

    Add files via upload

---
+++ demo/index/assets/js/dialog.js
@@ -0,0 +1,12 @@
+
+		/*彈跳視窗*/
+		var dialog,dialog1;
+		window.onload=function(){
+			dialog=document.getElementById("dialog");
+		};
+		function showDialog(){
+			dialog.style.display="block";
+		};
+		function closeDialog(){
+			dialog.style.display="none";
+		};
